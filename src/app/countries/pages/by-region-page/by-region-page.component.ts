9eddad7da233910eca4639f97e3e7221a7ffeaa3
JANACA <jaumenadaldev@gmail.com>
2024-06-25 17:06:37 +0200

    Afegir persistencia i optimitzar la cerca

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-import { Country } from '../../interfaces/country';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
+
+import { Country } from '../../interfaces/country.interface';
+import { Region } from '../../interfaces/region.type';
+
+
 
 @Component({
   selector: 'countries-by-region-page',
@@ -9,17 +13,31 @@ import { CountriesService } from '../../services/countries.service';
 
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit{
 
     public countries: Country[] = [];
+    public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+    public selectedRegion?: Region;
+    public isLoading: boolean = false;
 
     constructor( private countriesService: CountriesService ) { }
 
-    searchByRegion( region: string ):void  {
-      this.countriesService.searchRegion( region )
-        .subscribe( countries => {
-          this.countries = countries;
-      });
+    ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
-    }
+  searchByRegion( region: Region ):void  {
+
+    this.isLoading = true;
+
+    this.selectedRegion = region;
+
+    this.countriesService.searchRegion( region )
+      .subscribe( countries => {
+        this.countries = countries;
+        this.isLoading = false;
+    });
+
+  }
 }
